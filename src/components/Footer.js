3d5022fd33805e54789b4c003991b32562333e24
Tej7278-chippada <tejachippada45@gmail.com>
2024-11-02 00:30:16 +0530

    Working perfect on UPI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div>
-        <Box sx={{ bgcolor: 'background.paper', p: 3, mt: 'auto', textAlign: 'center' }}>
-        {/* <Typography variant="body2" color="textSecondary">
+        {/* <Box sx={{ bgcolor: 'background.paper', p: 3, mt: 'auto', textAlign: 'center' }}>
+        <Typography variant="body2" color="textSecondary">
             &copy; Tej Pay 2024
-        </Typography> */}
-        </Box>
+        </Typography>
+        </Box> */}
         <Box sx={{ textAlign: "center", py: 2 }}>
         <Typography variant="body2" color="textSecondary">
         &copy; 2024 Tej Pay |{" "}
