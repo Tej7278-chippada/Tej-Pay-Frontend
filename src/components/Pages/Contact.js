fbdb65e923f36d90511bfaec8f2ffe43d71935a1
Tej7278-chippada <tejachippada45@gmail.com>
2024-11-02 01:43:24 +0530

    Header, Footer added on Terms&Conditions pages

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,17 +1,26 @@
 // src/pages/Contact.js
 import React from 'react';
 import { Box, Typography, Container } from '@mui/material';
+import Header from '../Header';
+import Footer from '../Footer';
 
 const Contact = () => (
-  <Container>
-    <Box py={4}>
-      <Typography variant="h4">Contact Us</Typography>
-      <Typography variant="body1" paragraph>
-        If you have any questions or concerns, please feel free to reach out to us.
-      </Typography>
-      {/* Provide contact details or a form for inquiries */}
-    </Box>
-  </Container>
+  <div>
+    <Header/>
+    <Container>
+      <Box py={4}>
+        <Typography variant="h4">Contact Us</Typography>
+        <Typography variant="body1" paragraph>
+          If you have any questions or concerns, please feel free to reach out to us.
+        </Typography>
+        {/* Provide contact details or a form for inquiries */}
+        <Typography variant="body1" paragraph>
+          Mail to: tejachippada15@gmail.com
+        </Typography>
+      </Box>
+    </Container>
+    <Footer/>
+  </div>
 );
 
 export default Contact;
--- src/components/Pages/ShippingDelivery.js
+++ src/components/Pages/ShippingDelivery.js
@@ -1,17 +1,23 @@
 // src/pages/ShippingDelivery.js
 import React from 'react';
 import { Box, Typography, Container } from '@mui/material';
+import Header from '../Header';
+import Footer from '../Footer';
 
 const ShippingDelivery = () => (
-  <Container>
-    <Box py={4}>
-      <Typography variant="h4">Shipping and Delivery Policy</Typography>
-      <Typography variant="body1" paragraph>
-        This policy outlines our shipping and delivery process.
-      </Typography>
-      {/* Add details about your shipping and delivery policies */}
-    </Box>
-  </Container>
+  <div>
+    <Header/>
+    <Container>
+      <Box py={4}>
+        <Typography variant="h4">Shipping and Delivery Policy</Typography>
+        <Typography variant="body1" paragraph>
+          This policy outlines our shipping and delivery process.
+        </Typography>
+        {/* Add details about your shipping and delivery policies */}
+      </Box>
+    </Container>
+    <Footer/>
+  </div>
 );
 
 export default ShippingDelivery;
